88c0e8f433e2dc684d00a336c1581cb970f3b105
KushibikiMashu <kushibiki3413@gmail.com>
2020-07-12 06:51:12 +0900

    Step3: Concurrent Mode

--- index.js
+++ index.js
@@ -38,6 +38,24 @@ function render(element, container) {
   container.appendChild(dom)
 }
 
+let nextUnitOfWork = null
+
+function workLoop(deadline) {
+  let shouldYield = false
+  while(nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = perfomUnitOfWork(nextUnitOfWork)
+    shouldYield = deadline.timeRemaining() < 1
+  }
+
+  requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop)
+
+function perfomUnitOfWork(nextUnitOfWork) {
+  // TODO
+}
+
 const Didact = {
   createElement,
   render,
