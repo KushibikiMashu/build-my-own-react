d3a8973f640b93e18b8e0e48e2a164a06927d645
KushibikiMashu <kushibiki3413@gmail.com>
2020-07-12 06:21:21 +0900

    前準備

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-const element = <h1 title="foo">Hello</h1>
+const element = {
+  type: "h1", // tagName
+  props: {
+    title: "foo",
+    children: "Hello",
+  },
+}
+
 const container = document.getElementById('root')
-ReactDOM.render(element, container)
+
+const node = document.createElement(element.type) // renderされる側
+node["title"] = element.props.title
+
+const text = document.createTextNode("")
+text["nodeValue"] = element.props.children
+
+node.appendChild(text)
+container.appendChild(node)
