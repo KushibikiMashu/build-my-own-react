c0310911aa0a1e4fa10957a2229877b8cb60d30e
KushibikiMashu <kushibiki3413@gmail.com>
2020-07-12 07:11:20 +0900

    Step4: Fiber

--- index.js
+++ index.js
@@ -21,28 +21,36 @@ function createTextElement(text) {
   }
 }
 
-function render(element, container) {
-  const dom = element.type === "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(element.type)
+function createDom(fiber) {
+  const dom = fiber.type === "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(fiber.type)
 
   // attributes、text_elementを追加する
   const isProperty = key => key !== "children"
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isProperty)
     .forEach(name => {
-        dom[name] = element.props[name]
+        dom[name] = fiber.props[name]
       }
     )
 
-  element.props.children.forEach(child => render(child, dom))
+  return dom
+}
 
-  container.appendChild(dom)
+function render(element, container) {
+  // set next unit of work
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    }
+  }
 }
 
 let nextUnitOfWork = null
 
 function workLoop(deadline) {
   let shouldYield = false
-  while(nextUnitOfWork && !shouldYield) {
+  while (nextUnitOfWork && !shouldYield) {
     nextUnitOfWork = perfomUnitOfWork(nextUnitOfWork)
     shouldYield = deadline.timeRemaining() < 1
   }
@@ -52,8 +60,59 @@ function workLoop(deadline) {
 
 requestIdleCallback(workLoop)
 
-function perfomUnitOfWork(nextUnitOfWork) {
-  // TODO
+function perfomUnitOfWork(fiber) {
+  // add dom node
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom)
+  }
+
+  // childごとに新しいfiberを作成する
+  const elements = fiber.props.children
+  let index = 0
+  let prevSibling = null
+
+  while (index < elements.length) {
+    const element = elements[index]
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber, // fiberは親の情報も持っておく
+      dom: null
+    }
+
+    // fiber treeに追加
+    if (index === 0 ){
+      // fiberの子供
+      fiber.child = newFiber
+    } else {
+      // indexが1以上なら、indexが0のfiberの兄弟
+      prevSibling.sibling = newFiber
+    }
+
+    prevSibling = newFiber
+    index++
+  }
+
+  // return next unit of work
+  // 子供があれば、子供を見る
+  // 子供がなく、兄弟があれば兄弟を見る
+  // 子供も兄弟もなければ、親の兄弟、つまり叔父を見る
+  if (fiber.child) {
+    return fiber.child
+  }
+
+  let nextFiber = fiber
+  while(nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling
+    }
+    nextFiber = nextFiber.parent
+  }
 }
 
 const Didact = {
