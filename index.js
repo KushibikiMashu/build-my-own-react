e5af2b94ead67e8c8a79665614a7c84255cd8692
KushibikiMashu <kushibiki3413@gmail.com>
2020-07-12 06:14:32 +0900

    ファイルを分割

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// const element = <h1 title="foo">Hello</h1>
-// const container = document.getElementById('root')
-// render(element, container)
-
-// const domContainer = document.querySelector('#like_button_container');
-// ReactDOM.render(e(LikeButton), domContainer);
-
+const element = <h1 title="foo">Hello</h1>
+const container = document.getElementById('root')
+ReactDOM.render(element, container)
