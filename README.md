9d1da45ac1f4c9612144ae8e97b810f67bd8f189
KushibikiMashu <kushibiki3413@gmail.com>
2020-07-12 06:11:51 +0900

    first-step

---
+++ README.md
@@ -0,0 +1,3 @@
+# Build my own react
+Try to build my own React
+https://pomb.us/build-your-own-react/
---
+++ index.js
@@ -0,0 +1,7 @@
+// const element = <h1 title="foo">Hello</h1>
+// const container = document.getElementById('root')
+// render(element, container)
+
+// const domContainer = document.querySelector('#like_button_container');
+// ReactDOM.render(e(LikeButton), domContainer);
+
