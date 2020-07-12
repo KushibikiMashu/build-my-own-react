b833d6b8b73faa1552e88d2b2935627aee85b17b
KushibikiMashu <kushibiki3413@gmail.com>
2020-07-12 19:19:20 +0900

    コメントを追加

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ function workLoop(deadline) {
   let shouldYield = false
   while (nextUnitOfWork && !shouldYield) {
     nextUnitOfWork = perfomUnitOfWork(nextUnitOfWork)
+
     shouldYield = deadline.timeRemaining() < 1
   }
 
@@ -70,11 +71,11 @@ function perfomUnitOfWork(fiber) {
     fiber.parent.dom.appendChild(fiber.dom)
   }
 
-  // childごとに新しいfiberを作成する
   const elements = fiber.props.children
   let index = 0
   let prevSibling = null
 
+  // childrenを全てfiberにする
   while (index < elements.length) {
     const element = elements[index]
 
@@ -86,11 +87,12 @@ function perfomUnitOfWork(fiber) {
     }
 
     // fiber treeに追加
-    if (index === 0 ){
+    if (index === 0) {
       // fiberの子供
       fiber.child = newFiber
     } else {
       // indexが1以上なら、indexが0のfiberの兄弟
+      // <b></b>のみが入る
       prevSibling.sibling = newFiber
     }
 
@@ -99,18 +101,20 @@ function perfomUnitOfWork(fiber) {
   }
 
   // return next unit of work
-  // 子供があれば、子供を見る
-  // 子供がなく、兄弟があれば兄弟を見る
-  // 子供も兄弟もなければ、親の兄弟、つまり叔父を見る
+  // 子供があれば、子供を返す
+  // 子供がなく、兄弟があれば兄弟を返す
+  // 子供も兄弟もなければ、親の兄弟、つまり叔父を返す
   if (fiber.child) {
     return fiber.child
   }
 
   let nextFiber = fiber
-  while(nextFiber) {
+  while (nextFiber) {
     if (nextFiber.sibling) {
+      // 兄弟要素、もしくは親の兄弟を抜き出す
       return nextFiber.sibling
     }
+    // rootの時は、parentがないのでundefinedになり、loopを抜ける
     nextFiber = nextFiber.parent
   }
 }
