43491a5b9bffca5cb98a8df3041e74af81f5235a
KushibikiMashu <kushibiki3413@gmail.com>
2020-07-13 23:04:58 +0900

    Step6: Reconciliation

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function createElement(type, props, ...children) {
   return {
-    type,
+    type, // HTMLタグかTEXT_ELEMENT
     props: {
       ...props,
       children: children.map(child =>
-        typeof child === "object" ? child : createTextElement(child)
+        typeof child == "object" ? child : createTextElement(child)
       ),
     },
   }
@@ -24,32 +24,80 @@ function createTextElement(text) {
 function createDom(fiber) {
   const dom = fiber.type === "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(fiber.type)
 
-  // attributes、text_elementを追加する
-  const isProperty = key => key !== "children"
-  Object.keys(fiber.props)
-    .filter(isProperty)
-    .forEach(name => {
-        dom[name] = fiber.props[name]
-      }
-    )
+  updateDom(dom, {}, fiber.props)
 
   return dom
 }
 
+// eventのプロパティを判別する
+const isEvent = key => key.startsWith('on')
+const isProperty = key => key !== "children" && !isEvent(key)
+const isNew = (prev, next) => key => prev[key] !== next[key]
+const isGone = (prev, next) => key => !(key in next)
+
+function updateDom(dom, prevProps, nextProps) {
+  // remove old or changed event listeners
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .filter(key => !(key in nextProps) || isNew(prevProps, nextProps)(key))
+    .forEach(name => {
+      const eventType = name
+        .toLowerCase()
+        .substring(2)
+      dom.removeEventListener(eventType, prevProps[name])
+    })
+
+  // remove old properties
+  Object.keys(prevProps)
+    .filter(isProperty)
+    .filter(isGone(prevProps, nextProps))
+    .forEach(name => {
+      dom[name] = ""
+    })
+
+  // set new changed properties
+  Object.keys(nextProps)
+    .filter(isProperty)
+    .filter(isNew(prevProps, nextProps))
+    .forEach(name => {
+      dom[name] = nextProps[name]
+    })
+
+  // add event listeners
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .filter(isNew(prevProps, nextProps))
+    .forEach(name => {
+      const eventType = name
+        .toLowerCase()
+        .substring(2)
+      dom.addEventListener(eventType, nextProps[name])
+    })
+}
+
 // 全てのnodeをDOMに再帰的に追加する
 function commitRoot() {
+  deletions.forEach(commitWork)
   commitWork(wipRoot.child)
+  currentRoot = wipRoot
   wipRoot = null
 }
 
 function commitWork(fiber) {
-  if(!fiber) {
+  if (!fiber) {
     return
   }
 
-  // fiberの親のDOMにnodeを追加する（自分のDOMを親のDOMにappendChildする）
   const domParent = fiber.parent.dom
-  domParent.appendChild(fiber.dom)
+
+  // fiberの親のDOMにnodeを追加する（自分のDOMを親のDOMにappendChildする）
+  if (fiber.effectTag === "PLACEMENT" && fiber.dom != null) {
+    domParent.appendChild(fiber.dom)
+  } else if (fiber.effectTag === "UPDATE" && fiber.dom != null) {
+    updateDom(fiber.dom, fiber.alternate.props, fiber.props)
+  } else if (fiber.effectTag === "DELETION") {
+    domParent.removeChild(fiber.dom)
+  }
 
   // 子要素、兄弟要素を探索する
   commitWork(fiber.child)
@@ -62,13 +110,17 @@ function render(element, container) {
     dom: container,
     props: {
       children: [element],
-    }
+    },
+    alternate: currentRoot, // 以前のfiber
   }
+  deletions = []
   nextUnitOfWork = wipRoot
 }
 
 let nextUnitOfWork = null
+let currentRoot = null
 let wipRoot = null
+let deletions = null
 
 function workLoop(deadline) {
   let shouldYield = false
@@ -96,33 +148,7 @@ function performUnitOfWork(fiber) {
   }
 
   const elements = fiber.props.children
-  let index = 0
-  let prevSibling = null
-
-  // childrenを全てfiberにする
-  while (index < elements.length) {
-    const element = elements[index]
-
-    const newFiber = {
-      type: element.type,
-      props: element.props,
-      parent: fiber, // fiberは親の情報も持っておく
-      dom: null
-    }
-
-    // fiber treeに追加
-    if (index === 0) {
-      // fiberの子供
-      fiber.child = newFiber
-    } else {
-      // indexが1以上なら、indexが0のfiberの兄弟
-      // <b></b>のみが入る
-      prevSibling.sibling = newFiber
-    }
-
-    prevSibling = newFiber
-    index++
-  }
+  reconcileChildren(fiber, elements)
 
   // return next unit of work
   // 子供があれば、子供を返す
@@ -143,19 +169,89 @@ function performUnitOfWork(fiber) {
   }
 }
 
+// wipFiberは古いfiber。elementsは新しいもの。この関数でfiberとelementsを比較する
+function reconcileChildren(wipFiber, elements) {
+  let index = 0
+  let oldFiber = wipFiber.alternate && wipFiber.alternate.child
+  let prevSibling = null
+
+  // childrenを全てfiberにする
+  while (index < elements.length || oldFiber != null) {
+    const element = elements[index]
+    let newFiber = null
+
+    // compare oldFiber to element
+    // HTMLタグの比較
+    const sameType = oldFiber && element && element.type == oldFiber.type
+
+    // keyを指定すると配列の要素がのindexが変更されたことを検出するパフォーマンスが上がる
+    if (sameType) {
+      // update the node
+      newFiber = {
+        type: oldFiber.type,
+        props: element.props, // propsだけを更新する
+        dom: oldFiber.dom,
+        parent: wipFiber, // fiberは親の情報も持っておく
+        alternate: oldFiber,
+        effectTag: "UPDATE",
+      }
+    }
+    if (element && !sameType) {
+      // add the node
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        dom: null,
+        parent: wipFiber,
+        alternate: null,
+        effectTag: "PLACEMENT",
+      }
+    }
+    if (oldFiber && !sameType) {
+      // delete the oldFiber's node
+      oldFiber.effectTag = "DELETION"
+      deletions.push(oldFiber)
+    }
+
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling
+    }
+
+    // fiber treeに追加
+    if (index === 0) {
+      // fiberの子供
+      wipFiber.child = newFiber
+    } else if (element) {
+      // indexが1以上なら、indexが0のfiberの兄弟
+      // <b></b>のみが入る
+      prevSibling.sibling = newFiber
+    }
+
+    prevSibling = newFiber
+    index++
+  }
+}
+
 const Didact = {
   createElement,
   render,
 }
 
-/** @jsx Didact.createElement */
-const element = (
-  <div id="foo">
-    <a>bar</a>
-    <b/>
-  </div>
-)
-
 const container = document.getElementById("root")
-Didact.render(element, container)
 
+const updateValue = e => {
+  rerender(e.target.value)
+}
+
+const rerender = value => {
+  /** @jsx Didact.createElement */
+  const element = (
+    <div>
+      <input type="input" value={value} onInput={updateValue}/>
+      <h2>Hello {value}</h2>
+    </div>
+  )
+  Didact.render(element, container)
+}
+
+rerender("World")
